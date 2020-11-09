15f556e95ec8bb2fc2ee0e3f39122d1e16ef499b
Sheldon <chizhandesheldon689@gmail.com>
2020-11-09 23:46:45 +0200

    enable auto config

--- src/main/java/zw/co/invenico/springcommonsmodule/SpringCommonsModuleApplication.java
+++ src/main/java/zw/co/invenico/springcommonsmodule/SpringCommonsModuleApplication.java
@@ -1,13 +1,13 @@
 package zw.co.invenico.springcommonsmodule;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+
+@Configuration
+@ComponentScan(basePackages = "zw.co.invenico.springcommonsmodule")
+@EnableJpaAuditing
 public class SpringCommonsModuleApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(SpringCommonsModuleApplication.class, args);
-    }
 
 }
