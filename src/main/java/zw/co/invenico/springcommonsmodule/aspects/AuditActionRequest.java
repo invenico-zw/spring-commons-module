b18873976cb111749c3faf6a80e329a57f0f8ec4
Sheldon <chizhandesheldon689@gmail.com>
2020-12-05 09:52:14 +0200

    refactoring audit

--- src/main/java/zw/co/invenico/springcommonsmodule/aspects/AuditActionRequest.java
+++ src/main/java/zw/co/invenico/springcommonsmodule/aspects/AuditActionRequest.java
@@ -1,16 +1,27 @@
 package zw.co.invenico.springcommonsmodule.aspects;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuditActionRequest {
+
+    private String username;
 
     private String resource;
 
-    private String action;
+    private String actionPerformed;
 
-    public AuditActionRequest(String resource, String action) {
-        this.resource = resource;
-        this.action = action;
-    }
+    private LocalDateTime dateTime;
+
+    private String clientIpAddress;
+
+    private String serverIpAddress;
 }
--- src/main/java/zw/co/invenico/springcommonsmodule/aspects/AuditTrailManagementAspect.java
+++ src/main/java/zw/co/invenico/springcommonsmodule/aspects/AuditTrailManagementAspect.java
@@ -1,12 +1,20 @@
 package zw.co.invenico.springcommonsmodule.aspects;
 
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Objects;
 
 @Slf4j
 @Component
@@ -24,8 +32,24 @@ public class AuditTrailManagementAspect {
 
         log.info("## action: {} resource: {}", audit.action(), audit.resource());
 
+        String clientIPAddress = null;
+
+        String remoteIPAddress = null;
+
+        String username = null;
+
         try {
             Object result = joinPoint.proceed();
+
+            HttpServletRequest request =
+                    ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes()))
+                            .getRequest();
+
+            clientIPAddress = request.getRemoteAddr();
+
+            remoteIPAddress = request.getLocalAddr();
+
+            username = getUsername();
 
             log.info("The method {}() ends with {}", joinPoint.getSignature().getName(), result);
 
@@ -34,15 +58,45 @@ public class AuditTrailManagementAspect {
             log.error("Illegal argument {} in {}()", Arrays.toString(joinPoint.getArgs()),
                     joinPoint.getSignature().getName());
 
+            HttpServletRequest request =
+                    ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes()))
+                            .getRequest();
+
+            clientIPAddress = request.getRemoteAddr();
+
+            remoteIPAddress = request.getLocalAddr();
+
+            username = getUsername();
             throw e;
         } finally {
+            String finalClientIPAddress = clientIPAddress;
+            String finalRemoteIPAddress = remoteIPAddress;
+            String finalUsername = username;
             new Thread(() -> {
-
-                Object response = auditFeignClientService.create(new AuditActionRequest(audit.resource(), audit.action()));
+                val auditActionRequest = AuditActionRequest.builder()
+                        .actionPerformed(audit.action())
+                        .resource(audit.resource())
+                        .clientIpAddress(finalClientIPAddress)
+                        .dateTime(LocalDateTime.now())
+                        .serverIpAddress(finalRemoteIPAddress)
+                        .username(finalUsername)
+                        .build();
+                Object response = auditFeignClientService.create(auditActionRequest);
 
                 log.info("### response {}", response);
             }).start();
         }
+    }
+
+    private String getUsername() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails) principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+        return username;
     }
 
 }
